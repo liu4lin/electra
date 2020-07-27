a43d72783be08203a0cf9df147d2839e11d8a5fe
Shilin Liu <liushilin@bbdservice.com>
2020-07-27 11:01:36 +0800

    update run glue script

--- run_glue.sh
+++ run_glue.sh
@@ -1,15 +1,16 @@
 source ~/venv3.5.2-tf.1.15.0/bin/activate
-python3 run_finetuning.py --data-dir data --model-name electra_small_owt_v10 --hparams '{"model_size": "small", "task_names": ["cola"]}'
+python3 run_finetuning.py --data-dir data --model-name electra_small_owt_v10.l512 --hparams '{"model_size": "small", "task_names": ["cola"]}'
 wait
-#python3 run_finetuning.py --data-dir data --model-name electra_small_owt_v10 --hparams '{"model_size": "small", "task_names": ["sst"]}'
-python3 run_finetuning.py --data-dir data --model-name electra_small_owt_v10 --hparams '{"model_size": "small", "task_names": ["mrpc"]}'
+python3 run_finetuning.py --data-dir data --model-name electra_small_owt_v10.l512 --hparams '{"model_size": "small", "task_names": ["sst"]}'
 wait
-python3 run_finetuning.py --data-dir data --model-name electra_small_owt_v10 --hparams '{"model_size": "small", "task_names": ["sts"]}'
+python3 run_finetuning.py --data-dir data --model-name electra_small_owt_v10.l512 --hparams '{"model_size": "small", "task_names": ["mrpc"]}'
 wait
-python3 run_finetuning.py --data-dir data --model-name electra_small_owt_v10 --hparams '{"model_size": "small", "task_names": ["qqp"]}'
+python3 run_finetuning.py --data-dir data --model-name electra_small_owt_v10.l512 --hparams '{"model_size": "small", "task_names": ["sts"]}'
 wait
-python3 run_finetuning.py --data-dir data --model-name electra_small_owt_v10 --hparams '{"model_size": "small", "task_names": ["mnli"]}'
+python3 run_finetuning.py --data-dir data --model-name electra_small_owt_v10.l512 --hparams '{"model_size": "small", "task_names": ["qqp"]}'
 wait
-python3 run_finetuning.py --data-dir data --model-name electra_small_owt_v10 --hparams '{"model_size": "small", "task_names": ["qnli"]}'
+python3 run_finetuning.py --data-dir data --model-name electra_small_owt_v10.l512 --hparams '{"model_size": "small", "task_names": ["mnli"]}'
 wait
-python3 run_finetuning.py --data-dir data --model-name electra_small_owt_v10 --hparams '{"model_size": "small", "task_names": ["rte"]}'
+python3 run_finetuning.py --data-dir data --model-name electra_small_owt_v10.l512 --hparams '{"model_size": "small", "task_names": ["qnli"]}'
+wait
+python3 run_finetuning.py --data-dir data --model-name electra_small_owt_v10.l512 --hparams '{"model_size": "small", "task_names": ["rte"]}'
